ce6a1014c077cdaae10f736f0d4414cacb3b24dc
Martin Georgiev Georgiev <mggeorgiev@gmail.com>
2020-05-16 00:58:42 +0300

    Create Powerpoint method

---
+++ PowerPointProjectPlannerWebApp/Models/ProjectModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PowerPointProjectPlannerWebApp.Models
+{
+    public class ProjectModel
+    {
+        [MaxLength(50), MinLength(3)]
+        public string Title { get; set; }
+        
+        [MaxLength(50), MinLength(3), DataType("string")]
+        public string Description { get; set; }
+
+        public int Interval { get; set; }
+        public int Duration { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
